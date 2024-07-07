dce67a76595c1f18e1f58611e65ab28e370e0900
Franco Ponticelli <franco.ponticelli@gmail.com>
2024-07-06 22:21:24 -0600

    Add scaffold for @tempots/ui

--- packages/tempots-ui/scripts/make.cjs
+++ packages/tempots-ui/scripts/make.cjs
@@ -6,12 +6,7 @@ const {
 
 function update(mode) {
   const cwd = process.cwd()
-  const newVersion = updateLibVersion(cwd, mode)
-  const dependencies = ['tempots-ssr', 'tempots-ui'].map(name => path.join(cwd, `../${name}`))
-  for(const dep of dependencies) {
-    updateDependencies(newVersion, '@tempots/dom', dep)
-  }
-  return newVersion
+  return updateLibVersion(cwd, mode)
 }
 
 function publish() {
