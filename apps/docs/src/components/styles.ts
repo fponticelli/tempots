72ed74df3476625a5ae0f443dc18eb879bb2696f
Franco Ponticelli <franco.ponticelli@gmail.com>
2024-07-12 21:13:32 -0600

    remove dark prose for now

--- apps/docs/src/components/styles.ts
+++ apps/docs/src/components/styles.ts
@@ -19,5 +19,6 @@ export const Styles = {
       'flex h-6 w-8 shrink-0 items-center justify-center rounded border border-gray-400 bg-white text-[0.625rem] font-medium text-gray-600 group-hover:border-blue-600 group-hover:text-blue-600',
   },
   prose:
-    'prose prose-stone prose-md dark:prose-invert prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-h4:text-lg prose-h5:text-lg prose-h6:text-lg',
+    'prose prose-stone prose-md prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-h4:text-lg prose-h5:text-lg prose-h6:text-lg',
+  //  dark:prose-invert
 }
