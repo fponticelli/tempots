37ed1570db3f4726b422cb479d1e4ac65156f7ab
Franco Ponticelli <franco.ponticelli@gmail.com>
2024-07-15 20:48:39 -0600

    Change DOMEl to DOMNode

--- packages/tempots-dom/src/renderable/domnode.ts
+++ packages/tempots-dom/src/renderable/domnode.ts
@@ -1,11 +1,11 @@
 import { DOMContext } from '../dom/dom-context'
 import { removeDOMNode } from '../dom/dom-utils'
 
-export const DOMEl = (element: Element) => (ctx: DOMContext) => {
-  ctx.appendOrInsert(element)
+export const DOMNode = (node: Node) => (ctx: DOMContext) => {
+  ctx.appendOrInsert(node)
   return (removeTree: boolean) => {
     if (removeTree) {
-      removeDOMNode(element)
+      removeDOMNode(node)
     }
   }
 }
