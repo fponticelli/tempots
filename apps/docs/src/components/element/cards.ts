7db5c6282970d2bd7e53c2a3c48af856913a7b90
Franco Ponticelli <franco.ponticelli@gmail.com>
2024-09-13 20:03:47 -0600

    Fix demos page

--- apps/docs/src/components/element/cards.ts
+++ apps/docs/src/components/element/cards.ts
@@ -2,7 +2,9 @@ import { attr, TNode, html } from '@tempots/dom'
 
 export function Cards(items: TNode[], ...children: TNode[]) {
   return html.ul(
-    attr.class('w-full h-full justify-center flex flex-row flex-wrap gap-4'),
+    attr.class(
+      'w-full max-h-full justify-center flex flex-row flex-wrap gap-4'
+    ),
     ...items.map(item => html.li(attr.class('w-72'), item)),
     ...children
   )
